e28df45bf811e8fb28165923208a137321cd7c68
AJ-Kulundu <jameskulundu@gmail.com>
2020-11-17 16:45:46 +0300

    Single Page Application Part 2

--- src/components/DishdetailComponent.jsx
+++ src/components/DishdetailComponent.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import {Card, CardBody, CardImg, CardText, CardTitle} from 'reactstrap';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import {BreadcrumbItem,Breadcrumb, Card, CardBody, CardImg, CardText, CardTitle} from 'reactstrap';
+import {Link} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
     function RenderDish({dish}){
         if(dish!=null)
          return(
-             <div className="container">
-             <div className= "row">
             <div className="col-12 col-md-5 m-1">
                 <Card>
                     <CardImg width="100%" src={dish.image} alt= {dish.name}/>
@@ -17,13 +16,6 @@ import 'bootstrap/dist/css/bootstrap.min.css'
                      </CardBody>
                 </Card>
             </div>
-            <div className="col-12 col-md-5 m-1">
-              <h4>Comments</h4>
-              <RenderComments  comments = {dish.comments} />
-            </div>
-            </div>
-            </div>
-    
          );
         else
         return(
@@ -41,9 +33,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
                   <li>--{co.author}, {new Intl.DateTimeFormat("en-US", {year: 'numeric', month:'short', day:'2-digit'}).format(new Date(Date.parse(co.date)))}</li><br></br>
                   </React.Fragment>
                   )
-              })
-          
-
+              });
           return(
               <ul className = "list-unstyled">
                   {com}
@@ -53,13 +43,28 @@ import 'bootstrap/dist/css/bootstrap.min.css'
         else{
             return(<div></div>);
         }
-
     }
-
+    
     const Dishdetail = (props)=>{
         return(
-         <div className="row">
-             <RenderDish dish = {props.dish}/>
+         <div className="container">
+             <div className="row">
+                 <Breadcrumb>
+                 <BreadcrumbItem><Link to = "/menu">Menu</Link></BreadcrumbItem>
+                 <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                 </Breadcrumb>
+                 <div className="col-12">
+                     <h3>{props.dish.name}</h3>
+                     <hr/>
+                 </div>
+             </div>
+             <div className="row">
+                <RenderDish dish={props.dish} />
+            <div className="col-12 col-md-5 m-1">
+              <h4>Comments</h4>
+              <RenderComments  comments = {props.comments} />
+            </div>
+            </div>
          </div>
         );
     }
--- src/components/MenuComponent.jsx
+++ src/components/MenuComponent.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import {Card,CardImgOverlay,CardImg,CardTitle} from 'reactstrap';
+import {Card,CardImgOverlay,CardImg,CardTitle, BreadcrumbItem, Breadcrumb} from 'reactstrap';
+import {Link} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function RenderMenuItem({dish, onClick}){
         return(
-        <Card key = {dish.id} onClick= {()=> onClick(dish.id)}>
-            <CardImg width="100%" src={dish.image} alt = {dish.name}/>
+        <Card>
+            <Link to = {`/menu/${dish.id}`} >
+             <CardImg width="100%" src={dish.image} alt = {dish.name}/>
                 <CardImgOverlay>
                     <CardTitle>
                         {dish.name}
                     </CardTitle>
                 </CardImgOverlay>
+            </Link>
          </Card> 
         );
 }
@@ -27,6 +30,16 @@ const Menu = (props) =>{
 
         return(
             <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                      <BreadcrumbItem><Link to = "/home">Home</Link></BreadcrumbItem>
+                      <BreadcrumbItem active>Menu</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>Menu</h3>
+                        <hr/>
+                    </div>
+                </div>
                <div className="row">
                    {menu}
                </div>
