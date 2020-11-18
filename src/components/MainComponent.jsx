941e83c8b57d42705ef3d491395894a50d1dca52
AJ-Kulundu <jameskulundu@gmail.com>
2020-11-18 13:16:15 +0300

    Assignment 2

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -10,6 +10,7 @@ import {COMMENTS} from '../shared/comments';
 import {PROMOTIONS} from '../shared/promotions';
 import {LEADERS} from '../shared/leaders';
 import Dishdetails from './DishdetailComponent';
+import About from './AboutComponent';
 
 
 class Main extends Component {
@@ -52,6 +53,7 @@ class Main extends Component {
         <Route  exact path = "/menu" component = {() => <Menu dishes = {this.state.dishes} />}/>
         <Route exact path = "/contactus" component = {Contact} />
         <Route path = "/menu/:dishId" component={DishWithId} />
+        <Route path= "/aboutus" component={() => <About leaders= {this.state.leaders}/>} />
       {/* <Dishdetails dish ={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />*/}
        <Redirect to = "/home" />
        </Switch>
