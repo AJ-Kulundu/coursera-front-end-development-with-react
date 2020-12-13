e1287623fdfcf6efbcc08ffc91cf33d63c77d7e7
AJ-Kulundu <jameskulundu@gmail.com>
2020-12-13 21:53:46 +0300

    Fetch from json server

--- src/components/DishdetailComponent.jsx
+++ src/components/DishdetailComponent.jsx
@@ -5,6 +5,7 @@ import {Link} from 'react-router-dom';
 import {Control,LocalForm,Errors} from 'react-redux-form';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Loading} from './LoadingComponent';
+import {baseUrl} from '../shared/baseUrl'
 
 const required = (val) => val && val.length;
 const minLength =(len) => (val) => !(val) || (val.length>= len);
@@ -109,7 +110,7 @@ class  CommentForm extends Component {
          return(
             <div className="col-12 col-md-5 m-1">
                 <Card key={dish.id}>
-                    <CardImg width="100%" src={dish.image} alt= {dish.name}/>
+                    <CardImg width="100%" src={baseUrl + dish.image} alt= {dish.name}/>
                      <CardBody>
                          <CardTitle>{dish.name}</CardTitle>
                          <CardText>{dish.description}</CardText>
