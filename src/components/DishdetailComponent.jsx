e46aa89ef824b27c571171361d026aede6551c7c
AJ-Kulundu <jameskulundu@gmail.com>
2020-12-21 20:25:39 +0300

    React Animation Components

--- src/components/DishdetailComponent.jsx
+++ src/components/DishdetailComponent.jsx
@@ -5,7 +5,8 @@ import {Link} from 'react-router-dom';
 import {Control,LocalForm,Errors} from 'react-redux-form';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Loading} from './LoadingComponent';
-import {baseUrl} from '../shared/baseUrl'
+import {baseUrl} from '../shared/baseUrl';
+import {FadeTransform, Fade, Stagger } from 'react-animation-components'
 
 const required = (val) => val && val.length;
 const minLength =(len) => (val) => !(val) || (val.length>= len);
@@ -117,13 +118,18 @@ class  CommentForm extends Component {
         if(dish!=null)
          return(
             <div className="col-12 col-md-5 m-1">
-                <Card key={dish.id}>
+                <FadeTransform in 
+                    transformProps={{
+                     exitTransform: 'scale(0.5) translateY(-50%) '
+                     }}>
+                    <Card key={dish.id}>
                     <CardImg width="100%" src={baseUrl + dish.image} alt= {dish.name}/>
-                     <CardBody>
-                         <CardTitle>{dish.name}</CardTitle>
-                         <CardText>{dish.description}</CardText>
-                     </CardBody>
-                </Card>
+                        <CardBody>
+                            <CardTitle>{dish.name}</CardTitle>
+                            <CardText>{dish.description}</CardText>
+                        </CardBody>
+                    </Card>
+                </FadeTransform>
             </div>
          );
         else
@@ -137,16 +143,20 @@ class  CommentForm extends Component {
 
               const com = comments.map(co =>{
                   return(
+                  <Fade in>
                   <React.Fragment>
                   <li>{co.comment}</li><br></br>
                   <li>--{co.author}, {new Intl.DateTimeFormat("en-US", {year: 'numeric', month:'short', day:'2-digit'}).format(new Date(Date.parse(co.date)))}</li><br></br>
                   </React.Fragment>
+                  </Fade>
                   )
               });
           return(
               <ul className = "list-unstyled">
+                  <Stagger in>
                   {com}
                   <CommentForm dishId ={dishId} postComment={postComment}/> 
+                  </Stagger>
               </ul>
           );
         }
